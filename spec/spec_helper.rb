e28b709c9aad74ca2925df4a63fb7186b0ed20b0
Bart Kamphorst <bart@kamphorst.com>
2013-09-17 10:35:01 +0200

    Removed fixtures dir with test data (no longer needed, as it comes with external adapter specs).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,3 @@ gem_spec = Gem::Specification.find_by_name('gollum_git_adapter_specs')
 gem_spec_dir = "#{gem_spec.gem_dir}/#{gem_spec.require_paths[0]}"
 Dir.glob("#{gem_spec_dir}/**/*.rb").each {|spec| require "#{spec}"}
 
-def fixture(name)
-  File.join(File.dirname(__FILE__), 'fixtures', name)
-end
