113815412a50ba9f56a8a98b6c2a05144c4a29a4
Bart Kamphorst <bart@kamphorst.com>
2014-08-18 09:38:34 +0200

    Now testing against gollum/adapter_specs.

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gemspec
 gem 'rake', '~> 10.0.3'
 
-gem 'gollum_git_adapter_specs', :git => 'https://github.com/repotag/gollum_git_adapter_specs.git'
+gem 'adapter_specs', :git => 'https://github.com/gollum/adapter_specs.git'
 
 group :test do
   gem "rspec", "2.13.0"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ SimpleCov.start
 
 require 'gollum-lib_grit_adapter'
 
-gem_spec = Gem::Specification.find_by_name('gollum_git_adapter_specs')
+gem_spec = Gem::Specification.find_by_name('adapter_specs')
 gem_spec_dir = "#{gem_spec.gem_dir}/#{gem_spec.require_paths[0]}"
 Dir.glob("#{gem_spec_dir}/**/*.rb").each {|spec| require "#{spec}"}
 
