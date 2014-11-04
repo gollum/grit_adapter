eb25d8db51014d0952b2c7147707008bce03ee17
Dawa Ometto <dawa.ometto@phil.uu.nl>
2014-11-05 00:17:04 +0100

    Add -I flag to Git#grep.

--- lib/grit_adapter/git_layer_grit.rb
+++ lib/grit_adapter/git_layer_grit.rb
@@ -128,8 +128,8 @@ module Gollum
       
       def grep(query, options={})
         ref = options[:ref] ? options[:ref] : "HEAD"
-        args = [{}, '-i', '-c', query, ref, '--']
-        args << '--' << options[:path] if options[:path]
+        args = [{}, '-I', '-i', '-c', query, ref, '--']
+        args << options[:path] if options[:path]
         result = @git.grep(*args).split("\n")
         result.map do |line|
           branch_and_name, _, count = line.rpartition(":")
