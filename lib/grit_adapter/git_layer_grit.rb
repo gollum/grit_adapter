b38bd9e84584a3e7143f685ffaaa9e1f90ad80a1
C-Abner <chuangchita@gmail.com>
2017-10-05 05:48:25 +0000

    [Fix bug] verbose_grep
    partition method -> split

--- lib/grit_adapter/git_layer_grit.rb
+++ lib/grit_adapter/git_layer_grit.rb
@@ -145,8 +145,7 @@ module Gollum
         args << options[:path] if options[:path]
         result = @git.native(:grep, *args).split("\n")
         result.map do |line|
-          branch_and_name, _, count = line.rpartition(":")
-          branch, _, name = branch_and_name.partition(':')
+          branch, name, count = line.split(":", 3)
           {:name => name, :count => count}
         end
       end
